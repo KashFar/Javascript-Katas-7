a5536d08102e833c91bf2c67938e6a46594a3b3a
Khashayar Farhadi <kashfarhadi@gmail.com>
2018-12-13 10:56:33 -0500

    added initial files

---
+++ script.js
@@ -0,0 +1,23 @@
+// Your task will be to reproduce the behavior of these Array methods with your own 
+// functions – without using JavaScript's built-in versions:
+
+// 1. forEach()
+
+
+// 2. map()
+// 3. some()
+// 4. find()
+// 5. findIndex()
+// 6. every()
+// 7. filter()
+a
+
+// concat()
+// includes()
+// slice()
+// indexOf()
+// join()
+// slice()
+// flat()
+// flatMap()
+// Array.of()
